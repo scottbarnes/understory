8b875ec83f23d5d774b2606fb700a050b219aa82
scottbarnes <scottreidbarnes@gmail.com>
2021-07-16 23:00:06 -0700

    Working on automated tests.

--- setup_env.sh
+++ setup_env.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-echo understory=production >> .env
-echo DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE >> .env
-echo DJANGO_SECRET_KEY=$DJANGO_SECRET_KEY >> .env
-echo DJANGO_ALLOWED_HOSTS=$DJANGO_ALLOWED_HOSTS >> .env
+echo understory=production >> .django
+echo DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE >> .django
+echo DJANGO_SECRET_KEY=$DJANGO_SECRET_KEY >> .django
+echo DJANGO_ALLOWED_HOSTS=$DJANGO_ALLOWED_HOSTS >> .django
 
-echo db_name=$db_name >> .env
-echo db_user=$db_user >> .env
-echo db_pass=$db_pass >> .env
-echo db_host=$db_host >> .env
-echo db_port=$db_port >> .env
+echo db_name=$db_name >> .django
+echo db_user=$db_user >> .django
+echo db_pass=$db_pass >> .django
+echo db_host=$db_host >> .django
+echo db_port=$db_port >> .django
 
-echo POSTGRES_DB=$POSTGRES_DB >> .env
-echo POSTGRES_USER=$POSTGRES_USER >> .env
-echo POSTGRES_PASSWORD=$POSTGRES_PASSWORD >> .env
-echo POSTGRES_HOST=$POSTGRES_HOST >> .env
-echo POSTGRES_PORT=$POSTGRES_PORT >> .env
+echo POSTGRES_DB=$POSTGRES_DB >> .django
+echo POSTGRES_USER=$POSTGRES_USER >> .django
+echo POSTGRES_PASSWORD=$POSTGRES_PASSWORD >> .django
+echo POSTGRES_HOST=$POSTGRES_HOST >> .django
+echo POSTGRES_PORT=$POSTGRES_PORT >> .django
 
 # django-slack
-echo DJANGO_SLACK_TOKEN=$DJANGO_SLACK_TOKEN >> .env
-echo DJANGO_SLACK_CHANNEL=$DJANGO_SLACK_CHANNEL >> .env
+echo DJANGO_SLACK_TOKEN=$DJANGO_SLACK_TOKEN >> .django
+echo DJANGO_SLACK_CHANNEL=$DJANGO_SLACK_CHANNEL >> .django
