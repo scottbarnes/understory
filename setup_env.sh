efda153aef69c2df6af8d9516c213df2e491d492
Scott Barnes <scottreidbarnes@gmail.com>
2022-04-07 20:33:49 -0700

    Working on Github Actions.

---
+++ setup_env.sh
@@ -0,0 +1,5 @@
+echo POSTGRES_DB=$POSTGRES_DB >> .envs/.production/.django
+echo POSTGRES_USER=$POSTGRES_USER >> .envs/.production/.django
+echo POSTGRES_PASSWORD=$POSTGRES_PASSWORD >> .envs/.production/.django
+echo POSTGRES_HOST=$POSTGRES_HOST >> .envs/.production/.django
+echo POSTGRES_PORT=$POSTGRES_PORT >> .envs/.production/.django
